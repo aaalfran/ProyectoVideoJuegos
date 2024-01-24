95c7d2041ce6acdebab83082f3e3ca1c7f1502de
aaalfran <aaalfran@espol.edu.ec>
2024-01-24 03:38:33 -0500

    Se agrego movimiento de guardias en vertical

--- scripts/policiabasico.gd
+++ scripts/policiabasico.gd
@@ -3,34 +3,33 @@ extends CharacterBody2D
 var movespeed = 100
 var has_adjusted_position = false
 var restablish = false
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity.x = movespeed
 	move_and_slide()
 	
 	if is_on_wall():
 		movespeed = -movespeed
-		has_adjusted_position = false  # Restablece la variable cuando cambia la dirección
-	# Ajusta la posición del cono visual solo cuando la dirección del movimiento cambia
+		has_adjusted_position = false 
+		
 	if restablish and not has_adjusted_position:
 		$Visualcone.position.x = $Visualcone.position.x + 290
-		print($Visualcone.position.x)
 		restablish = false
+		
+	#El personaje se mueve hacia la derecha
 	if movespeed > 0 and not has_adjusted_position:
 		$AnimatedSprite2D.flip_h = false
 		$Visualcone.flip_h = false
-		has_adjusted_position = true  # Marca que la posición ha sido ajustada
-		print($Visualcone.position.x)
+		has_adjusted_position = true 
+	#El personaje se mueve hacia la izquierda
 	elif movespeed < 0 and not has_adjusted_position:
 		$AnimatedSprite2D.flip_h = true
 		$Visualcone.flip_h = true
 		$Visualcone.position.x = $Visualcone.position.x - 290
-		print($Visualcone.position.x)
-		has_adjusted_position = true  # Marca que la posición ha sido ajustada
+		has_adjusted_position = true  
 		restablish = true
 	
 
---
+++ scripts/policiabasico2.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+var movespeed = 100
+var has_adjusted_position2 = false
+var restablish2 = false
+
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity.y = movespeed
+	move_and_slide()
+	has_adjusted_position2 = false
+	
+	if restablish2 and not has_adjusted_position2:
+		$VisualCone2.position.y = $VisualCone2.position.y + 290
+		restablish2 = false
+	
+	#El personaje se mueve hacia abajo
+	if is_on_ceiling()  and not has_adjusted_position2:
+		$AnimatedSprite2D.play("mov_abajo")
+		$VisualCone2.flip_h = false
+		movespeed = -movespeed
+		has_adjusted_position2 = true 
+		restablish2 = true
+		
+	#El personaje se mueve hacia arriba
+	elif is_on_floor() and not has_adjusted_position2:
+		$AnimatedSprite2D.play("mov arriba")
+		$VisualCone2.flip_h = true
+		$VisualCone2.position.y = $VisualCone2.position.y -290
+		print($VisualCone2.position.y)
+		movespeed = -movespeed
+		has_adjusted_position2 = true 
+		
+		
+		
+		
+
