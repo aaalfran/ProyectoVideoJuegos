33bc2ea1e71698a4fec23db0e5fababdddbdbc27
pedbvasq <pedbvasq@espol.edu.ec>
2024-01-27 22:52:41 -0500

    llave add

--- Escenas/Area2D.gd
+++ Escenas/Area2D.gd
@@ -1,8 +1,12 @@
 extends Area2D
-
+var llave = false
 # Señal que se emite cuando hay una colisión
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") and llave ==true:
 		get_tree().change_scene_to_file("res://Escenas/CellBlock.tscn")
 
+
+
+func _on_llave_puerta_llave():
+	llave =true
