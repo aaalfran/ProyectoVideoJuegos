e5c6ef9876803a3d03f4aca402ea39400eb11a5f
pedbvasq <pedbvasq@espol.edu.ec>
2024-01-29 22:31:23 -0500

    label puerta

--- Escenas/BlockArea.gd
+++ Escenas/BlockArea.gd
@@ -15,7 +15,16 @@ func _process(delta):
 func _on_body_entered(body):
 	if body.is_in_group("Player") and llave ==true:
 		get_tree().change_scene_to_file("res://Escenas/Patio.tscn")
-
+	else:
+		if body.is_in_group("Player"):
+			$interactuarP.visible=true
+		
 
 func _on_llave_puerta_llave():
 	llave =true
+
+
+func _on_body_exited(body):
+	if body.is_in_group("Player"):
+			$interactuarP.visible=false
+	
